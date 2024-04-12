7206e1664589963a944e4585f941c2470c67afff
Ajay <ajayshvkadam98@gmail.com>
2024-04-12 19:42:28 +0530

    jarvis

---
+++ engine/db.py
@@ -0,0 +1,42 @@
+import csv
+import sqlite3
+
+conn = sqlite3.connect("jarvis.db")
+cursor = conn.cursor()
+#query = "CREATE TABLE IF NOT EXISTS sys_command(id integer primary key, name VARCHAR(100), path VARCHAR(1000))"
+#cursor.execute(query)
+
+#query = "INSERT INTO sys_command VALUES (null,'word', 'C:\\Program Files\\Microsoft Office\\root\\Office16\\WINWORD.exe')"
+#cursor.execute(query)
+#conn.commit()
+
+#query = "CREATE TABLE IF NOT EXISTS web_command(id integer primary key, name VARCHAR(100), url VARCHAR(1000))"
+#cursor.execute(query)
+
+#query = "INSERT INTO web_command VALUES (null,'spotify', 'https://open.spotify.com/')"
+#cursor.execute(query)
+#conn.commit()
+
+#Create a table with the desired columns
+#cursor.execute('''CREATE TABLE IF NOT EXISTS contacts (id integer primary key, name VARCHAR(200), mobile_no VARCHAR(255), email VARCHAR(255) NULL)''')
+
+# Specify the column indices you want to import (0-based index)
+# Example: Importing the 1st and 3rd columns
+#desired_columns_indices = [0, 30]
+
+# # Read data from CSV and insert into SQLite table for the desired columns
+#with open('contacts.csv', 'r', encoding='utf-8') as csvfile:
+#    csvreader = csv.reader(csvfile)
+#    for row in csvreader:
+#        selected_data = [row[i] for i in desired_columns_indices]
+#       cursor.execute(''' INSERT INTO contacts (id, 'name', 'mobile_no') VALUES (null, ?, ?);''', tuple(selected_data))
+
+#query = "INSERT INTO contacts VALUES (null,'V', '+917389508614', 'null')"
+#cursor.execute(query)
+#query = 'V'
+#query = query.strip().lower()
+#conn.commit()
+
+#cursor.execute("SELECT mobile_no FROM contacts WHERE LOWER(name) LIKE ? OR LOWER(name) LIKE ?", ('%' + query + '%', query + '%'))
+#results = cursor.fetchall()
+#print(results[0][0]) 
